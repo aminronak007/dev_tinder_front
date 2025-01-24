4f9424d931f156c791e0747a0c4851cb5a2e694e
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2025-01-24 17:59:48 +0530

    Integrate Socket connection

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,27 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { createSocketConnection } from "../../utils/socket";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  const [messages, setMessages] = useState([
-    {
-      text: "Hello",
-      from: "me",
-    },
-    {
-      text: "Hi",
-      from: "me",
-    },
-  ]);
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+    setNewMessage("");
+  };
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const socket = createSocketConnection();
+    // As soon as the page loads, join the chat
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      targetUserId,
+      userId,
+    });
+
+    socket.on("getMessage", ({ firstName, newMessage: text }) => {
+      setMessages((prevMessages) => [...prevMessages, { firstName, text }]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [userId, targetUserId]);
 
   return (
-    <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[75vh] flex flex-col">
+    <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[75vh] flex flex-col">
       <h1 className="p-5 border-b border-gray-600">Chat</h1>
       <div className="flex-1 overflow-y-scroll p-5">
         {messages.map((msg, index) => {
           return (
             <div key={index}>
-              <div className="chat chat-start">
+              <div
+                className={
+                  msg?.firstName === user?.firstName
+                    ? "chat chat-end"
+                    : "chat chat-start"
+                }
+              >
                 <div className="chat-image avatar">
                   <div className="w-10 rounded-full">
                     <img
@@ -31,35 +66,25 @@ const Chat = () => {
                   </div>
                 </div>
                 <div className="chat-header">
-                  Obi-Wan Kenobi
+                  {msg.firstName}
                   <time className="text-xs opacity-50">12:45</time>
                 </div>
-                <div className="chat-bubble">You were the Chosen One!</div>
+                <div className="chat-bubble">{msg.text}</div>
                 <div className="chat-footer opacity-50">Delivered</div>
-              </div>
-              <div className="chat chat-end">
-                <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
-                    <img
-                      alt="Tailwind CSS chat bubble component"
-                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                    />
-                  </div>
-                </div>
-                <div className="chat-header">
-                  Anakin
-                  <time className="text-xs opacity-50">12:46</time>
-                </div>
-                <div className="chat-bubble">I hate you!</div>
-                <div className="chat-footer opacity-50">Seen at 12:46</div>
               </div>
             </div>
           );
         })}
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
-        <input className="flex-1 broder border-gray-500 text-black  rounded p-2 bg-slate-400" />
-        <button className="btn btn-primary">Send</button>
+        <input
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="flex-1 broder border-gray-500 text-black rounded p-2 bg-slate-400"
+        />
+        <button onClick={sendMessage} className="btn btn-primary">
+          Send
+        </button>
       </div>
     </div>
   );
